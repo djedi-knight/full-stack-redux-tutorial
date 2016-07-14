429c5369146fdc67958cde95f0e0960fd350903e
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-07-14 12:20:50 -0400

    added src/core.js and associated test code

---
+++ test/core_spec.js
@@ -0,0 +1,47 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setEntries, next} from '../src/core';
+
+describe('Application Logic', () => {
+
+  describe('setEntries()', () => {
+
+    it('Adds the Entries to the State', () => {
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 Days Later');
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+    it('Converts to Immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+  });
+
+  describe('next()', () => {
+
+    it('Takes the Next Two Entries Under Vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    });
+
+  });
+
+});
